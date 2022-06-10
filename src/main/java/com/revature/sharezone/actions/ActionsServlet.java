126a54a2142ee8236ffb9771a0d35c3f6b40969b
Romane Orlando Robb <mail@romanerobb.com>
2022-06-09 22:21:00 -0400

    Made changes.

--- src/main/java/com/revature/sharezone/actions/ActionsServlet.java
+++ src/main/java/com/revature/sharezone/actions/ActionsServlet.java
@@ -1,8 +1,15 @@
 package com.revature.sharezone.actions;
 
+import com.revature.sharezone.userprofile.UserProfile;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+
+import javax.swing.*;
 
 @RestController
 @CrossOrigin
@@ -14,6 +21,13 @@ public class ActionsServlet {
     public ActionsServlet(ActionsServices actionsServices) {
         this.actionsServices = actionsServices;
     }
+
+    @PostMapping("/actions")
+    public ResponseEntity<UserProfile> saveActions(@RequestBody Actions actions){
+        Actions newActions = actionsServices.create(actions);
+        return new ResponseEntity<>(newActions, HttpStatus.CREATED);
+    }
+
 
 
 }
